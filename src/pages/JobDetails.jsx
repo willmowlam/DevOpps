5acf01abb25119a8b301b002fd2491cbc1a74a7a
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2025-10-09 21:18:01 +0100

    Refactor job details for compatability with job data from Bing api endpoint

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -4,6 +4,11 @@ import parse from 'html-react-parser';
 function JobDetails(props) {
 
   let data = props.selectedJob;
+  
+  // Safety check
+  if (!data) {
+    return <div>No job data available</div>;
+  }
 
   // Add to favourites 
 
@@ -44,49 +49,6 @@ function JobDetails(props) {
     localStorage.setItem("favourites", JSON.stringify(favourites));
   }, [favourites]);
 
-  // Format description text
-  function formatDescription(text) {
-
-    if (typeof text === 'string') {
-
-      // Parse html characters
-      text = parse(text);
-
-      // Define the bullet character regex
-      const bulletRegex = /•/;
-
-      // Split the text based on the bullet character
-      const points = text.split(bulletRegex);
-
-      // Format bullet points and paragraphs
-      const processBullets = points.map((point, index) => {
-        // No bullet at the beginning
-        if (index === 0) {
-          return point.trim();
-        } else {
-          return (<Fragment key={index}><br /><i className="fas fa-circle text-[8px] mr-2"></i>{point.trim()}</Fragment>);
-        }
-      });
-
-      // Format paragraphs looking for double lines 
-      const processParagraphs = processBullets.map((point) => {
-        if (typeof point === 'string') {
-          // Split double new lines into array
-          const paragraphs = point.split("\n\n");
-          // Format each array item into a paragraph
-          return paragraphs.map((paragraph, i) => (<p key={i} className="pb-4">{paragraph}</p>));
-        } else {
-          return point;
-        }
-      });
-
-      return processParagraphs;
-    } else {
-      return text;
-    }
-  }
-
-
   //window.scrollTo({ top: 0 });
 
   return (
@@ -94,24 +56,23 @@ function JobDetails(props) {
       <div className="w-full p-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
         <div className='flex flex-row'>
           <div>
-            <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{parse(data.title)}</h2>
-            <p><i className="fa-regular fa-building text-indigo-500"></i> <strong>Company:</strong> {parse(data.company)}</p>
-            <div className="my-4 font-normal text-gray-700 dark:text-gray-400">{formatDescription(data.description)}</div>
+            <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{data.title}</h2>
+            <p><i className="fa-regular fa-building text-indigo-500"></i> <strong>Company:</strong> {data.companyName}</p>
+            <div className="my-4 font-normal text-gray-700 dark:text-gray-400">{parse(data.descriptionHtml)}</div>
           </div>
           <div className="favourite" onClick={addToFavourites}><i className={isChecked ? "fa-solid fa-heart text-indigo-500 text-2xl" : "fa-regular fa-heart text-indigo-500 text-2xl"}></i></div>
         </div>
         <div className='flex flex-row sm:flex-col md:flex-row lg:flex-row justify-evenly flex-wrap gap-4 py-3 border-t-1 border-b-1'>
           <p><i className="fa-regular fa-compass text-indigo-500"></i> <strong>Location:</strong> {data.location}</p>
           <p><i className="fa-regular fa-clock text-indigo-500"></i> <strong>Employment Type:</strong> {data.employmentType}</p>
-          <p><i className="fa-regular fa-calendar text-indigo-500"></i> <strong>Date posted:</strong> {data.datePosted}</p>
-          <p><i className="fa-regular fa-money-bill-1 text-indigo-500"></i> <strong>Salary range:</strong> {data.salaryRange == "" ? "Competitive" : data.salaryRange}</p>
+          <p><i className="fa-regular fa-calendar text-indigo-500"></i> <strong>Date posted:</strong> {data.postedTimeAgo}</p>
         </div>
+        {data.applyUrl && (
         <div className='flex flex-row sm:flex-col md:flex-row lg:flex-row justify-evenly flex-wrap gap-4 py-3 border-b-1'>
           <h3 className="font-semibold">Apply here</h3>
-          {data.jobProviders.map((provider, index) => (
-            <p key={provider.jobProvider + '-' + index}><a href={provider.url} target='_blank' className='text-sm hover:text-indigo-400'>{provider.jobProvider} <i className="fa-regular fa-share-from-square text-indigo-500"></i></a></p>
-          ))}
+            <p><a href={data.applyUrl} target='_blank' className='text-sm hover:text-indigo-400'>APPLY <i className="fa-regular fa-share-from-square text-indigo-500"></i></a></p>
         </div>
+        )}
       </div>
     </>
   );
